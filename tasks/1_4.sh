22e12422446a6f8340747ea207d425e4ec7205eb
FlyinPancake <FlyinPancake@users.noreply.github.com>
2023-04-05 00:50:58 +0200

    updated script to account for different interface names
    Signed-off-by: FlyinPancake <FlyinPancake@users.noreply.github.com>

--- tasks/1_4.sh
+++ tasks/1_4.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
-
-local_network=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d. -f1-3).0/24
+lan_interface=$(ip addr show | grep "state UP" | awk '{print $2}' | cut -d: -f1 | grep -v lo)
+local_network=$(ip addr show "$lan_interface" | grep "inet\b" | awk '{print $2}' | cut -d. -f1-3).0/24
 
 sudo iptables -A INPUT -s "${local_network}" -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j DROP
